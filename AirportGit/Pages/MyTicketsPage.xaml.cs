ef31eaead9ee84d8fe1d5e7ba792d56459643330
Анна <annakudashova0902@gmail.com>
2023-12-04 00:06:07 +0300

    Страница с билетами входящего пользователя и отображением в нем информации о рейсах.

--- AirportGit/Pages/MyTicketsPage.xaml.cs
+++ AirportGit/Pages/MyTicketsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AirportGit.DB;
 
 namespace AirportGit.Pages
 {
@@ -20,9 +22,30 @@ namespace AirportGit.Pages
     /// </summary>
     public partial class MyTicketsPage : Page
     {
+        public static List<Ticket> tickets { get; set; }
+        public static List<Flight> flights { get; set; }
+        public static List<City> cities { get; set; }
         public MyTicketsPage()
         {
             InitializeComponent();
+            tickets = new List<Ticket>(DBConnection.airportEntities.Ticket.ToList().Where(x => x.IDClient == DBConnection.loginedClient.IDClient));
+            flights = new List<Flight>(DBConnection.airportEntities.Flight.ToList());
+            cities = new List<City>(DBConnection.airportEntities.City.ToList());
+            this.DataContext = this;
+        }
+
+        public void Refresh()
+        {
+            TicketsLv.ItemsSource = tickets;
+        }
+        private void FlightsLV_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BackBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainMenuClientPage());
         }
     }
 }
--- AirportGit/Pages/ViewFlightsPage.xaml.cs
+++ AirportGit/Pages/ViewFlightsPage.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,10 +23,12 @@ namespace AirportGit.Pages
     public partial class ViewFlightsPage : Page
     {
         public static List<Flight> flights { get; set; }
+        public static List<City> cities { get; set; }
         public ViewFlightsPage()
         {
             InitializeComponent();
             flights = new List<Flight>(DBConnection.airportEntities.Flight.ToList().Where(x => x.DepartureDate >= DateTime.Now));
+            cities = new List<City>(DBConnection.airportEntities.City.ToList());
             FlightsLV.ItemsSource = flights;
         }
 
@@ -39,6 +42,32 @@ namespace AirportGit.Pages
             {
                 NavigationService.Navigate(new AuthorizationPage());
             }
+        }
+        private void Refresh()
+        {
+        //    var filtred = DBConnection.airportEntities.City.ToList();
+
+        //    var name = City1FilterCB.SelectedItem as City;
+        //    if (City1FilterCB.SelectedIndex != 0 && name != null)
+        //    filtred = filtred.Where(x => x.Nazvanie == name.Nazvanie).ToList();
+        //    FlightsLV.ItemsSource = filtred.ToList();
+
+            var filtred = DBConnection.airportEntities.City.ToList();
+
+            var name = CBStudents.SelectedItem as City;
+            if (CBStudents.SelectedIndex != 0 && name != null)
+                filtred = filtred.Where(x => x.Nazvanie == name.Nazvanie).ToList();
+            FlightsLV.ItemsSource = filtred.ToList();
+        }
+
+        //private void City1FilterCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    Refresh();
+        //}
+
+        private void CBStudents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
         }
     }
 }
