f0e2e71db9b7ce60263bfde5ca66ee626835f4dd
Анна <annakudashova0902@gmail.com>
2023-12-03 15:12:56 +0300

    Сделала страницу с Самолетами, отображающую информацию о них.

--- AirportGit/Pages/AllAirplanesPage.xaml.cs
+++ AirportGit/Pages/AllAirplanesPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AirportGit.DB;
 
 namespace AirportGit.Pages
 {
@@ -20,9 +21,29 @@ namespace AirportGit.Pages
     /// </summary>
     public partial class AllAirplanesPage : Page
     {
+        public static List<Airplane> airplanes { get; set; }
+        public static List<Aircompany> aircompanies { get; set; }
+        public static List<Team> teams { get; set; }
+        public static List<AirplaneModel> airplaneModels { get; set; }
+        public static List<Worker> workers { get; set; }
+
+
         public AllAirplanesPage()
         {
             InitializeComponent();
+            airplanes = new List<Airplane>(DBConnection.airportEntities.Airplane.ToList());
+            aircompanies = new List<Aircompany>(DBConnection.airportEntities.Aircompany.ToList());
+            teams = new List<Team>(DBConnection.airportEntities.Team.ToList());
+            airplaneModels = new List<AirplaneModel>(DBConnection.airportEntities.AirplaneModel.ToList());
+            workers = new List<Worker>(DBConnection.airportEntities.Worker.ToList().Where(x => x.IDPosition == 1 || x.IDPosition == 6 || x.IDPosition == 3));
+            AirplanesLv.ItemsSource = new List<Airplane>(DBConnection.airportEntities.Airplane.ToList());
+
+
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainMenuWorkerPage());
         }
     }
 }
