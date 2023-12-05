999ea7f766fb818d512551056842f758f0988e74
Варя <varya.kornilova.21@inbox.ru>
2023-12-05 22:49:46 +0300

    Изменен комментарий в DBConnection

--- AirportGit/DB/DBConnection.cs
+++ AirportGit/DB/DBConnection.cs
@@ -11,7 +11,7 @@ namespace AirportGit.DB
         // База данных Аня
         //public static AirportEntities1 airportEntities = new AirportEntities1();
 
-        //База данных Варвары (Model 04)
+        //База данных Варвары (Model 12)
         //public static Airport03Entities1 airportEntities = new Airport03Entities1();
 
 
